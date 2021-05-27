abc4c20812c97ce95edc8db8a42d1e8832132ce7
priyasura <priyasura01@gmail.com>
2021-05-28 01:18:26 +0530

    commit

--- App.js
+++ App.js
@@ -25,35 +25,16 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
+import Welcome from '../Lets-Connect/Welcome/Welcome'
 class App extends React.Component{
   render(){
     return(
       <View>
-        <Text>Welcome to Let's Connect</Text>
-        <Text>Yahooooooooooooooooooooooooooooo</Text>
+        <Welcome></Welcome>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
