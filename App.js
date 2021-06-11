abc25b03c94f23057e0e1982e25d057fbad773a4
Karanam-Tejaswini <tejaswinikranam5@gamil.com>
2021-06-11 14:32:53 +0530

    lets-11/2

--- App.js
+++ App.js
@@ -1,3 +1,66 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow strict-local
+//  */
+
+// import React from 'react';
+// import 'react-native-gesture-handler';
+
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
+
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+// //Working with port 8087 ----- it's pretty good actually and having fun with this
+// import Signup from './mine/Signup';
+// import S from './hii/S'
+// import Stack from './navigation/stack';
+//  export default function App(){
+  
+//     return(
+//       // <View>
+//         // <S/>
+//       //   {/* <Signup/> */}
+//       // </View>
+//       <Stack />
+//     )
+  
+// }
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,59 +69,58 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import 'react-native-gesture-handler';
+ import React from 'react';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-//Working with port 8087 ----- it's pretty good actually and having fun with this
-import Signup from './mine/Signup';
-import S from './hii/S'
-import Stack from './navigation/stack';
- export default function App(){
-  
-    return(
-      // <View>
-        // <S/>
-      //   {/* <Signup/> */}
-      // </View>
-      <Stack />
-    )
-  
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ import Signup from './mine/Signup';
+ import S from './hii/S'
+ import Storage from './StorageDataBase/Storage'
+  export default function App(){
+   
+     return(
+       <View>
+         <Storage/>
+         {/* <S/> */}
+         {/* <Signup/> */}
+       </View>
+     )
+   
+ }
+ 
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+ });
+ 
+ 
+ 
